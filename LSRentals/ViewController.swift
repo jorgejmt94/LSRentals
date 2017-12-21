675b8b1a8c191c7820a83ac654639a58353395d8
Jorge Melguizo Torres <jorgejmt@macbook-pro-de-jorge-3.local>
2017-12-21 18:27:12 +0100

    Initial Commit

---
+++ LSRentals/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LSRentals
+//
+//  Created by Jorge Melguizo Torres on 21/12/17.
+//  Copyright © 2017 Albert i Jorge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
