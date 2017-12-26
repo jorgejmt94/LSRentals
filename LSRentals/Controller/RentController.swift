1710a17282a0b3a560e0fec3774a207e6ce63bd3
Jorge Melguizo Torres <jorgejmt@macbook-pro-de-jorge-3.local>
2017-12-26 21:38:44 +0100

    arreglar views + preparar table view

--- LSRentals/Controller/RentController.swift
+++ LSRentals/Controller/RentController.swift
@@ -10,4 +10,23 @@ import UIKit
 
 class RentController: UIViewController {
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated);
+        
+        //TODO: cargar info apartament (nombre y direccion, se podria hacer pasandola desde el anterior)
+    }
+    
+    @IBAction func cancelButton(_ sender: Any) {
+        //TODO: NO VA
+        self.navigationController?.popViewController(animated: true);
+
+    }
+    
+    @IBAction func rentButton(_ sender: Any) {
+        //TODO: envair reserva
+        
+        //anem a les pantallas dels tabs //TODO: NO VA
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
 }
